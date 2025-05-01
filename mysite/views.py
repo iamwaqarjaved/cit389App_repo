023c9018668fedfae271c51f05a36100abb969d5
waqarmart <waqarmart.com@gmail.com>
2025-05-01 18:53:29 -0400

    commit 2

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,3 @@
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse("Hello from Elastic Beanstalk Django App!")
+    return HttpResponse("Hello World! <br> Welcome to Elastic Beanstalk Django App!")
